2e713cdb130eb4fb48b0aa943f1bb185925b9e9e
hyeAkki <hye.akshay@gmail.com>
2019-10-02 11:41:25 +0000

    edited performance health.sh

--- minions/health_checkup.sh
+++ minions/health_checkup.sh
@@ -1,5 +1,5 @@
 for c in $(cat /etc/hosts | grep minion | awk {'print $2'})
 do
 echo $c
-    sshpass -e ssh -o StrictHostKeyChecking=no root@$c -p 21 'bash -s' < master/performance_monitor.sh
+    sshpass -e ssh -o StrictHostKeyChecking=no root@$c -p 21 'bash -s' < /home/madmin/kubernetes_configs/master/performance_monitor.sh
 done
